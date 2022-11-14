641b53c63d34cba8759e6f830a87d6f134c44a24
Joe Schnetzler <schnetzlerjoe@gmail.com>
2022-11-14 16:22:41 -0600

    Edit trade css

--- src/components/Transfer.svelte
+++ src/components/Transfer.svelte
@@ -1,21 +1,17 @@
 <script>
     import { Input } from "flowbite-svelte";
-    import { ArrowDown } from "svelte-heros"
 </script>
 
 <div class="swap-main-div">
     <div class="swap-div">
         <Input type="number" value=0 size="lg"></Input>
-        <div style="width: 100%; display: flex; justify-content: center;">
-            <div style="cursor: pointer; padding: 5px; border-radius: 10px;"><ArrowDown variation="outline"/></div>
-        </div>
         <Input type="number" value=0 size="lg"></Input>
-        <button class="trade-button" color="light">Trade</button>
+        <button class="trade-button" color="light">Transfer</button>
     </div>
 </div>
 
 <style>
-    .swap-main-div {
+    .transfer-main-div {
         margin-top: 20px;
         padding: 30px;
         display: flex;
@@ -23,7 +19,7 @@
         align-items: center;
         flex-wrap: wrap;
     }
-    .swap-div {
+    .transfer-div {
         padding: 30px;
         background-color: white;
         max-width: 500px;
@@ -35,7 +31,7 @@
         align-items: center;
         align-content: space-between;
     }
-    .trade-button {
+    .transfer-button {
         width: 100%;
         background-color: black;
         color: white;
